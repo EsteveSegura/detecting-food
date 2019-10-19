d0da3509de7c8b7ebde061c0026b5ad3f2433cef
Esteve Segura <estevesegura.gir@gmail.com>
2019-10-19 14:09:09 +0200

    cache OK

--- private/downloadVGG16.py
+++
@@ -1,6 +0,0 @@
-import keras
-from keras.applications.vgg16 import VGG16
-import tensorflowjs as tfjs
-model = VGG16(include_top=True, weights='imagenet')
-#model.save("VGG16.h5")
-tfjs.converters.save_keras_model(model,'./model')
