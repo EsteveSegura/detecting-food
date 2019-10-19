f534e0bdd9fe2f44bda33bf7b95df11165ed07c5
Esteve Segura <estevesegura.gir@gmail.com>
2019-10-20 01:32:04 +0200

    instagram workflow

--- src/detectFood.js
+++ src/detectFood.js
@@ -19,7 +19,8 @@ async function getPrediction(path){
 
 /*
 (async() => {
-     let prediction = await getPrediction('./imgs/testing2.jpg')
+     //let prediction = await getPrediction('./imgs/testing2.jpg')
+     let prediction = await getPrediction('https://scontent-mad1-1.cdninstagram.com/vp/398d97fc45103ad822d4a7ab6b4ccc8f/5E30FB0A/t51.2885-15/e35/71297284_429622957728811_8164393986194082260_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=107')
      console.log(prediction)
 })();
 */
--- src/instagram.js
+++ src/instagram.js
@@ -1,15 +1,38 @@
+const fs = require('fs');
+const download = require('image-downloader')
+const detectFood = require('./detectFood');
 require('tools-for-instagram');
+
+async function downloadPost(ig,media_id){
+     try {
+          let postToDownload = await getMediaIdInfo(ig,media_id);
+          await downloadImage(postToDownload.items[0].image_versions2.candidates[0].url,media_id);
+     } catch (error) {
+          return 'cant_reads';
+     }
+}
+
+async function downloadImage (url,id){
+     const path = `./imgs/${id}.jpg`
+     await download.image({url: url,dest: path})
+     return id;
+}
 
 
 (async () => {
-    let ig = await login();
-    await setAntiBanMode(ig);
-
-    setInterval(async () => {
-        let posts = await recentHashtagList(ig, "dogs");
-        await likePost(ig, posts[0]);
-        await getLikeActivityByHours(ig, 24);
-        console.log("Next It. in 75 seconds");
-    }, 1000 * 75);
+     let ig = await login();
+     await setAntiBanMode(ig);
+     
+     setInterval(async () => {
+          console.log('getin posts')
+          let posts = await recentHashtagList(ig, "foodporn");
+          console.log('posts done')
+          console.log('interating posts')
+          for(let i = 0 ; i < posts.length ; i++){
+               let actualPost = await downloadPost(ig,posts[i].pk)
+               let prediction = await detectFood.getPrediction(`./imgs/${actualPost}`)
+               console.log(`${actualPost}.jpg : ${prediction}`)
+          }
+     }, 60000 /* * 75*/);
 
 })();
