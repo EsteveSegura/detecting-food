df8dab89caf12e99162e09fba63aa85cc76338c4
Esteve Segura <estevesegura.gir@gmail.com>
2019-10-19 02:34:04 +0200

    call VGG16 from js

---
+++ src/detect.js
@@ -0,0 +1,24 @@
+const spawn = require('child_process').spawn;
+const ls = spawn('python', ['vgg.py', 'testing2.jpg']);
+
+function decodePrediction(str){
+     let decode = str.split('-');
+     return {
+          "label" : decode[0],
+          "percentage" : Math.trunc(decode[1])
+     }
+}
+
+async function callPython(){
+     return new Promise((resolve,reject)=>{
+          ls.stdout.on('data', (data) => {
+               resolve(decodePrediction(data.toString()))
+          });
+     })
+}
+
+(async() => {
+     let prediction = await callPython()
+     console.log(prediction)
+})();
+
---
+++ src/vgg.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import numpy as np
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+def predictPicture(picturePath):
+     mod = VGG16()
+     from PIL import Image as pil_image
+     img = pil_image.open('./' + picturePath)
+     img = img.resize((224, 224))
+     dataimg = np.float64(np.array(img))
+     dataimg = np.reshape(dataimg,(1,224,224,3))
+     dataimg = preprocess_input(dataimg)
+     pred = mod.predict(dataimg)
+     predlabel = decode_predictions(pred, top=3)
+     predlabel = predlabel[0][0]
+     return(predlabel[1] + "-" + str(predlabel[2]*100))
+
+predictedResult = predictPicture(str(sys.argv[1]))
+print(predictedResult)
