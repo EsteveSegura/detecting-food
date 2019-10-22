452159da6f4b4737aca53165231a56ac00e90659
Esteve Segura <estevesegura.gir@gmail.com>
2019-10-22 11:08:41 +0200

    bot final workflow

--- src/instagram.js
+++ src/instagram.js
@@ -1,10 +1,8 @@
-const fs = require('fs');
 const download = require('image-downloader')
 const detectFood = require('./detectFood');
-require('tools-for-instagram');
 
 //read txt file and convert to array
-function readFileLineByLineAndConvertToArray(dataPath){
+function readFileLineByLine(dataPath){
      return new Promise(function(resolve,reject){
          let dataset = []
  
@@ -22,48 +20,60 @@ function readFileLineByLineAndConvertToArray(dataPath){
      })
  }
 
+//download instagram post
 async function downloadPost(ig,media_id){
      try {
           let postToDownload = await getMediaIdInfo(ig,media_id);
           let downloadedData = await downloadImageFromUrl(postToDownload.items[0].image_versions2.candidates[0].url,media_id);
           return downloadedData;
      } catch (error) {
-          return 'cant_reads';
+          return 'cant_read';
      }
 }
 
+//Random int
+function randomInt(min,max){
+     return Math.round(Math.random() * (max-min)+ min)
+}
+
+//use package image-downloader for downloading images from the internet
 async function downloadImageFromUrl (url,id){
-     const path = `./imgs/${id}.jpg`
-     await download.image({url: url,dest: path})
+     const path = `./imgs/${id}.jpg`;
+     await download.image({url: url,dest: path});
      return id;
 }
 
+//this way can call python vggModel.py
 async function getPrediction(path){
-     let pred = await detectFood.getPrediction(path)
-     return pred
+     let pred = await detectFood.getPrediction(path);
+     return pred;
 }
 
+
+//instagram workflow
 (async () => {
-     let ig = await login();
-     await setAntiBanMode(ig);
+     let ig = await login(); //login
+     await setAntiBanMode(ig); //anti-ban Mode
      
-     //setInterval(async () => {
-          let posts = await recentHashtagList(ig, "foodporn");
-          console.log(posts.length)
+     //repeat this every hour
+     setInterval(async () => {
+          let posts = await recentHashtagList(ig, "foodporn"); //get all hashtags in #foodporn hashtag
           for(let i = 0 ; i < posts.length ; i++){
-               let actualPost = await downloadPost(ig,posts[i].pk)
+               let actualPost = await downloadPost(ig,posts[i].pk) //download picture
                if(actualPost != "no_preds"){
                     try {
-                         let prediction = await detectFood.getPrediction(`./imgs/${actualPost}.jpg`)
-                         console.log(`the media id: ${actualPost} is ${prediction.label} in ${prediction.percentage}.00%`)
+                         let prediction = await detectFood.getPrediction(`./imgs/${actualPost}.jpg`) //try to predict
+                         console.log(`the media id: ${actualPost} is ${prediction.label} in ${prediction.percentage}.00%`) 
                          if(prediction.label == "cheeseburger" && prediction.percentage >= 85){
+                              let sentences = readFileLineByLine('./burgerSentences.txt');
+                              let randomSentence = sentences[randomInt(0,sentences.length-1)]
+                              commentMediaId(ig, actualPost, randomSentence)
                               console.log("Comment that picture!")
                          }
                     } catch (error) {
-                         //console.log("sad")
+                         console.log("this is not a picture")
                     }
                }
           }
-     //}, 600000 /* * 75*/);
-
+     }, 60000);
 })();
